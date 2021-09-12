95b141dcc32af4f606a999e2c0e1b4921f995c11
Helder Matos <mtshelder@gmail.com>
2021-09-12 00:54:37 -0300

    Data is now represented by a list of booleans

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from src.data import Data
+
+d = Data(n_bits=8)
+print(d)
--- src/data.py
+++ src/data.py
@@ -1,23 +1,31 @@
+from random import choice
+
 # classe de representação dos dados
 class Data:
 
     # construtor
-    def __init__(self, bits=[]):
-        try:
-            self.bits = [b for b in bits] if self.verify_input(bits) else []
-        except TypeError:
-            print("TypeError: input must be a list of strings.")
-        except ValueError:
-            print("ValueError: bit must be either 0 or 1.")
+    def __init__(self, bits=[], n_bits=0):
+        if n_bits == 0:
+            try:
+                if self.verify_input(bits):
+                    self.bits = bits
+                else:
+                    self.bits = []
+            except TypeError:
+                print("TypeError: input must be a list of booleans.")
+            except ValueError:
+                print("ValueError: bit must be either True or False.")
+        else:
+            self.bits = [choice([True, False]) for _ in range(n_bits)]
 
     # vefica se a entrada é valida
     def verify_input(self, bits):
         if type(bits) is not list:
             raise TypeError("Input is not a list")
         for bit in bits:
-            if bit != '1' and bit != '0':
-                raise ValueError("Bit is not 1 or 0")
+            if (bit is not True) and (bit is not False):
+                raise ValueError("Bit must be a boolean.")
         return True
 
     def __str__(self):
-        return ''.join(self.bits)
+        return ''.join(["1" if b is True else "0" for b in self.bits])
